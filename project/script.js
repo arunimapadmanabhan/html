cc8053c5bec631d1d1bebe6bdc61a4972718414f
jithin_371 <jithin_371@bitbucket.org>
2018-06-17 21:56:30 +0530

    added

---
+++ project/script.js
@@ -0,0 +1,44 @@
+// Get the modal
+
+
+
+
+
+
+ function homeFunction() {
+	
+	var btn = document.getElementById("myBtn");
+	var home = document.getElementById('myHome');
+    home.style.display = "block";
+}
+function closeHome(){
+	var home = document.getElementById('myHome');
+	var span = document.getElementsByClassName("close")[0];
+    home.style.display = "none";
+}
+function aboutFunction(){
+	var btn = document.getElementById("myBtn");
+	var about = document.getElementById('myAbout');
+    about.style.display = "block";
+}
+function closeAbout(){
+	var about = document.getElementById('myAbout');
+	var span = document.getElementsByClassName("close")[0];
+	about.style.display = "none";
+}
+function contactFunction(){
+	var btn = document.getElementById("myBtn");
+	var contact= document.getElementById('myContact');
+    contact.style.display = "block";
+
+}
+function closecontact(){
+	var contact = document.getElementById('myContact');
+	var span = document.getElementsByClassName("close")[0];
+	contact.style.display = "none";
+}
+
+
+
+
+	
